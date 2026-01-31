43d4cbded555c4921201869c77fc5f2e2ea96f03
Lucas Momo <lucs.momo@gmail.com>
2026-01-31 11:24:29 +0000

    :sparkles: create resume list item

--- designsystem/build.gradle.kts
+++ designsystem/build.gradle.kts
@@ -60,12 +60,20 @@ android {
 }
 
 dependencies {
+    //Android dependencies
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
+
+    //Compose dependencies
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
+
+    //Preview dependencies
     implementation(libs.androidx.ui.tooling.preview)
+    debugImplementation(libs.ui.tooling)
+
+    //Material dependencies
+    implementation(libs.material)
     implementation(libs.androidx.material3)
 }
---
+++ designsystem/src/main/kotlin/lucas/momo/designsystem/ThemePreviews.kt
@@ -0,0 +1,8 @@
+package lucas.momo.designsystem
+
+import android.content.res.Configuration
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(name = "Light Mode", uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Preview(name = "Dark Mode", uiMode = Configuration.UI_MODE_NIGHT_YES)
+annotation class ThemePreviews
